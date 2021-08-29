6b070b3219c26a66395d8bbbe57d0a6ff705801f
Shreyansh <69718041+Shreyansh2006@users.noreply.github.com>
2021-08-30 01:15:40 +0530

    Update project.js

--- project.js
+++ project.js
@@ -5,11 +5,11 @@ const options = {weekday: 'long', year:'numeric', month: 'long', day: 'numeric'}
 setInterval(() => {
 a = new Date()
 date = a.toLocaleDateString(undefined, options);
-time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
+time = a.getHours() + ' : ' + a.getMinutes() + ': ' + a.getSeconds();
 document.getElementById('time').innerHTML = time + "<br>on " + date;
 }, 1000)
 
 // let sdf = new SimpleDateFormat("yyyy-MM-dd 'T' HH:mm:ss");
-// pakistan = TimeZone.getTimeZone("Asia/Karachi");
-// sdf.setTimeZone(pakistan);
+// pakistan = TimeZone.getTimeZone("Asia/Delhi");
+// sdf.setTimeZone(india);
 // MediaKeySystemAccess.err.println(sdf.format(new Date()));
